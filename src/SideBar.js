9250a5fd435cdd5fc94d474d7d86cbf342e307a3
Alejandro Soto <alexsotog82@gmail.com>
2021-08-21 23:10:49 -0500

    se sube clases del componente

--- src/SideBar.js
+++ src/SideBar.js
@@ -3,7 +3,28 @@ import { html, css, LitElement } from 'lit';
 export class SideBar extends LitElement {
   static get styles() {
     return css`
+      @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,600;1,400&display=swap');
       :host {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+      }
+      .sidebar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 260px;
+        background: #11101d;
+
+      }
+
+      .sidebar .logo-details {
+
+      }
+
+      .sidebar .logo-details .logo_name{
 
       }
     `;
